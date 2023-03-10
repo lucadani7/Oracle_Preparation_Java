445cd3c470eb9b3829c1eca66666aab58708ec56
lucadani7 <79052990+lucadani7@users.noreply.github.com>
2023-03-10 12:10:11 +0200

    Delete SimpleConversions directory

--- SimpleConversions/src/main/java/com/oracle/preparation/algorithm/Algorithm.java
+++
@@ -1,9 +0,0 @@
-package com.oracle.preparation.algorithm;
-
-public interface Algorithm {
-    String convertFromDecimalToBaseX(int num, int newBase) throws IllegalArgumentException;
-    int convertFromAnyBaseToDecimal(String num, int oldBase);
-    int charToDecimal(char c);
-    String convertNumberToNewBaseCustom(String num, int oldBase, int newBase);
-    char convertRemainderToLiteral(int num);
-}
